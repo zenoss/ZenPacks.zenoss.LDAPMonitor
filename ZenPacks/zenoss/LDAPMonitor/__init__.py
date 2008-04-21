1bb837e048ec2ca7a5690a289a361837b0fd6843
cgibbons <cgibbons@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2008-04-21 18:15:37 +0000

    * Fixes #2998 : new ZenPack for monitoring LDAP server response time using check_ldap or check_ldaps
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LDAPMonitor@22529 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/LDAPMonitor/__init__.py
@@ -0,0 +1,30 @@
+###########################################################################
+#
+# This program is part of Zenoss Core, an open source monitoring platform.
+# Copyright (C) 2008, Zenoss Inc.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version as published by
+# the Free Software Foundation.
+#
+# For complete information please visit: http://www.zenoss.com/oss/
+#
+###########################################################################
+
+import Globals
+import os.path
+
+skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
+from Products.CMFCore.DirectoryView import registerDirectory
+if os.path.isdir(skinsDir):
+    registerDirectory(skinsDir, globals())
+
+from Products.ZenModel.ZenPack import ZenPackBase
+
+class ZenPack(ZenPackBase):
+    packZProperties = [
+        ('zLDAPBaseDN', 'dc=zenoss,dc=com', 'string'),
+        ('zLDAPBindDN', '', 'string'),
+        ('zLDAPBindPassword', '', 'string'),
+    ]
+
