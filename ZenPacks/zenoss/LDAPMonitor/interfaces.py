ec27935a96c2e14f2ad56b4c2676c6fd8c7946ab
pwarren <pwarren@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-06-16 22:59:11 +0000

    Convert Text->TextLine / TextLine->Text for all schema types not modified since revision 41541. Reviewed by jgartman.
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LDAPMonitor@44253 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/LDAPMonitor/interfaces.py
+++ ZenPacks/zenoss/LDAPMonitor/interfaces.py
@@ -18,11 +18,11 @@ from Products.Zuul.utils import ZuulMessageFactory as _t
 class ILDAPMonitorDataSourceInfo(IRRDDataSourceInfo):
     cycletime = schema.Int(title=_t(u'Cycle Time (seconds)'))
     timeout = schema.Int(title=_t(u'Timeout (seconds)'))
-    ldapServer = schema.Text(title=_t(u'LDAP Server'), group=_t(u'LDAP'))
-    ldapBindDN = schema.Text(title=_t(u'Bind Distinguished Name'), group=_t(u'LDAP'))
+    ldapServer = schema.TextLine(title=_t(u'LDAP Server'), group=_t(u'LDAP'))
+    ldapBindDN = schema.TextLine(title=_t(u'Bind Distinguished Name'), group=_t(u'LDAP'))
     useSSL = schema.Bool(title=_t(u'Use SSL?'), group=_t(u'LDAP'))
-    ldapBaseDN = schema.Text(title=_t(u'Base Distinguished Name'), group=_t(u'LDAP'))
-    ldapBindVersion = schema.Text(title=_t(u'Bind Version'), group=_t(u'LDAP'))
+    ldapBaseDN = schema.TextLine(title=_t(u'Base Distinguished Name'), group=_t(u'LDAP'))
+    ldapBindVersion = schema.TextLine(title=_t(u'Bind Version'), group=_t(u'LDAP'))
     ldapBindPassword = schema.Password(title=_t(u'Bind Password'), group=_t(u'LDAP'))
     port = schema.Int(title=_t(u'Port'), group=_t(u'LDAP'))
     
