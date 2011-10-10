e47f861f49a2629dc31435cdecaf0cc742582461
jhanson <jhanson@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2011-10-10 13:32:16 +0000

    fixes #29183 reviewed by gsteiff previous commit had not upped the version number
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LDAPMonitor@48848 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # These variables are overwritten by Zenoss when the ZenPack is exported
 # or saved.  Do not modify them directly here.
 NAME = 'ZenPacks.zenoss.LDAPMonitor'
-VERSION = '1.2.4'
+VERSION = '1.3.0'
 AUTHOR = 'zenoss'
 LICENSE = ''
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.zenoss']
@@ -24,34 +24,34 @@ setup(
     version = VERSION,
     author = AUTHOR,
     license = LICENSE,
-    
+
     # This is the version spec which indicates what versions of Zenoss
     # this ZenPack is compatible with
     compatZenossVers = COMPAT_ZENOSS_VERS,
-    
+
     # previousZenPackName is a facility for telling Zenoss that the name
     # of this ZenPack has changed.  If no ZenPack with the current name is
     # installed then a zenpack of this name if installed will be upgraded.
-    prevZenPackName = PREV_ZENPACK_NAME, 
-    
+    prevZenPackName = PREV_ZENPACK_NAME,
+
     # Indicate to setuptools which namespace packages the zenpack
     # participates in
     namespace_packages = NAMESPACE_PACKAGES,
-    
+
     # Tell setuptools what packages this zenpack provides.
     packages = find_packages(),
-    
+
     # Tell setuptools to figure out for itself which files to include
     # in the binary egg when it is built.
     include_package_data = True,
-    
+
     # Tell setuptools what non-python files should also be included
     # with the binary egg.
-    package_data = { 
+    package_data = {
          '': ['*.txt'],
          NAME: ['objects/*','skins/*/*','services/*', 'reports/*/*',
                 'modeler/*/*', 'daemons/*', 'lib/*', 'libexec/*'],
-         },  
+         },
 
     # Indicate dependencies on other python modules or ZenPacks.  This line
     # is modified by zenoss when the ZenPack edit page is submitted.  Zenoss
@@ -68,5 +68,5 @@ setup(
     },
 
     # All ZenPack eggs must be installed in unzipped form.
-    zip_safe = False,    
+    zip_safe = False,
 )
