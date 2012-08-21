d4fc572648204f0574b432bbedede050630a5d4e
jcausey <jcausey@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-08-21 14:23:18 +0000

    second take at nagios warnings for collector installs; warns user about missing binaries when installing remote collectors; reviewed by jhanson
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.LDAPMonitor@63081 f5df01ce-0320-0410-9ed9-ddcd13c732cc

--- ZenPacks/zenoss/LDAPMonitor/__init__.py
+++ ZenPacks/zenoss/LDAPMonitor/__init__.py
@@ -1,6 +1,6 @@
 ##############################################################################
 # 
-# Copyright (C) Zenoss, Inc. 2008, all rights reserved.
+# Copyright (C) Zenoss, Inc. 2012, all rights reserved.
 # 
 # This content is made available according to terms specified in
 # License.zenoss under the directory where your Zenoss product is installed.
@@ -10,6 +10,9 @@
 
 import Globals
 import os.path
+
+import logging
+log = logging.getLogger("zen.LDAPMonitor")
 
 skinsDir = os.path.join(os.path.dirname(__file__), 'skins')
 from Products.CMFCore.DirectoryView import registerDirectory
@@ -24,3 +27,17 @@ class ZenPack(ZenPackBase):
         ('zLDAPBindDN', '', 'string'),
         ('zLDAPBindPassword', '', 'password'),
     ]
+
+def onCollectorInstalled(ob, event):
+    zpFriendly = 'LDAPMonitor'
+    errormsg = '{0} binary cannot be found on {1}. It must be installed before {2} can function.'
+    
+    verifyBin = 'check_ldap'
+    code, output = ob.executeCommand('zenbincheck %s' % verifyBin, 'zenoss', needsZenHome=True)
+    if code:
+       	log.warn(errormsg.format(verifyBin, ob.hostname, zpFriendly))
+    
+    verifyBin = 'check_ldaps'
+    code, output = ob.executeCommand('zenbincheck %s' % verifyBin, 'zenoss', needsZenHome=True)
+    if code:
+       	log.warn(errormsg.format(verifyBin, ob.hostname, zpFriendly))
